5b301974456994d9f03cd3ae907607148ca511d1
nourinawadd <nourinawad@gmail.com>
2025-07-07 12:59:57 +0300

    add task 4

---
+++ Day-2/Task-4.js
@@ -0,0 +1,29 @@
+let name = prompt("Enter your name: ");
+
+while (!name || name.trim() === "") {
+    name = prompt("Name cannot be empty. Please enter your name: ");
+}
+
+while(name.length <3) {
+    name = prompt("Name must be at least 3 characters long. Please enter your name: ");
+}
+
+let phone = prompt("Enter your phone number: ");
+while (!phone || phone.trim() === "") {
+    phone = prompt("Phone number cannot be empty. Please enter your phone number: ");
+}
+
+while (!/^\d{11}$/.test(phone)) {
+    phone = prompt("Phone number must be 11 digits long. Please enter your phone number: ");
+}
+
+let email = prompt("Enter your email: ");
+while (!email || email.trim() === "") {
+    email = prompt("Email cannot be empty. Please enter your email: ");
+}
+
+while (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    email = prompt("Invalid email format. Please enter a valid email: ");
+}
+
+console.log(`Welcome ${name}! Your phone number is ${phone} and your email is ${email}.`);
