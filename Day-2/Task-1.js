3c35deeaaf5b83a6ab11d13bc75843ad5a5336fc
nourinawadd <nourinawad@gmail.com>
2025-07-07 12:39:43 +0300

    add task 1

---
+++ Day-2/Task-1.js
@@ -0,0 +1,17 @@
+function sum(...args) {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] !== 'number' || isNaN(args[i])) {
+            return "Invalid input, all arguments must be numbers";
+        }
+        total += args[i];
+    }
+
+    return total;
+}
+
+// example usage
+console.log(sum(1, 2, 3));
+console.log(sum(1, 2, '3'));
+console.log(sum(1, 2, 'hi'));
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
