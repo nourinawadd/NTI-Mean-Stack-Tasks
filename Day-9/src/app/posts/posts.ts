38b22a7d9c49ef42396a573272efcc803e86bdb0
Nourin <nourinawad@gmail.com>
2025-07-21 22:31:22 +0300

    edit post content

--- Day-9/src/app/posts/posts.ts
+++ Day-9/src/app/posts/posts.ts
@@ -9,19 +9,20 @@ import { Ipost } from '../postModel';
 })
 export class Posts {
   posts: Ipost[] = [
-    {
-      title: 'Angular Basics',
-      body: 'Learning components, bindings, and directives.',
+    { title: 'Post 1',
+      body: 'Random text',
       id: 1,
       userid: 1,
-      username: 'nourinawad'
-    },
-    {
-      title: 'Angular Basics',
-      body: 'Learning components, bindings, and directives.',
+      username: 'nourinawad' },
+    { title: 'Post 2',
+      body: 'More random text',
       id: 2,
       userid: 2,
-      username: 'munaaldhshan'
-    }
+      username: 'munaaldhshan' },
+    { title: 'Post 3',
+      body: 'Even more random text',
+      id: 3,
+      userid: 3,
+      username: 'tamimawad' }
   ];
 }
