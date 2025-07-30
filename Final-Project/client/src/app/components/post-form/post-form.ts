b702342b94b00c1204412648763fb5255a37158b
nourinawadd <nourinawad@gmail.com>
2025-07-30 19:08:32 +0300

    create post form component

--- Final-Project/client/src/app/components/post-form/post-form.ts
+++ Final-Project/client/src/app/components/post-form/post-form.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { PostService } from '../../services/post';
 
 @Component({
   selector: 'app-post-form',
-  imports: [],
   templateUrl: './post-form.html',
-  styleUrl: './post-form.css'
+  styleUrls: ['./post-form.css']
 })
-export class PostForm {
+export class PostFormComponent {
+  content: string = '';
+  userId: string = 'demo-user-id'; // temp static user
 
+  constructor(private postService: PostService) {}
+
+  submitPost(): void {
+    if (!this.content.trim()) return;
+    this.postService.createPost(this.content, this.userId).subscribe(() => {
+      this.content = '';
+    });
+  }
 }
