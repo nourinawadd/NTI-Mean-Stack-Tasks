f5cb29026971047619ce662bd20b8a19a6c839dc
nourinawadd <nourinawad@gmail.com>
2025-07-30 19:06:05 +0300

    create post list component

--- Final-Project/client/src/app/components/post-list/post-list.ts
+++ Final-Project/client/src/app/components/post-list/post-list.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-post-list',
-  imports: [],
   templateUrl: './post-list.html',
-  styleUrl: './post-list.css'
+  styleUrls: ['./post-list.css']
 })
-export class PostList {
+export class PostListComponent implements OnInit {
+  posts: any[] = [];
+  userId: string = 'demo-user-id'; // temporary static user ID
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  loadPosts(): void {
+    this.postService.getPosts().subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
+  likePost(postId: string): void {
+    this.postService.likePost(postId, this.userId).subscribe(() => {
+      this.loadPosts();
+    });
+  }
+
+  deletePost(postId: string): void {
+    this.postService.deletePost(postId).subscribe(() => {
+      this.loadPosts();
+    });
+  }
+
+  comment(postId: string, commentText: string): void {
+    if (!commentText.trim()) return;
+    this.postService.addComment(postId, this.userId, commentText).subscribe(() => {
+      this.loadPosts();
+    });
+  }
 }
